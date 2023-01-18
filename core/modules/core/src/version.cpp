7a854678f14efa54478becc77f0eba8f449c2b4b
Mandar Harshe <mandar.harshe@klepsydra.com>
2023-01-18 11:20:13 +0100

    Set default emitter type to be safe.

--- core/modules/core/src/version.cpp
+++ core/modules/core/src/version.cpp
@@ -16,5 +16,5 @@
 
 const std::string KLEPSYDRA_CORE_VERSION_MAJOR = "7";
 const std::string KLEPSYDRA_CORE_VERSION_MINOR = "7";
-const std::string KLEPSYDRA_CORE_VERSION_PATCH = "1";
-const std::string KLEPSYDRA_CORE_VERSION = "7.7.1";
+const std::string KLEPSYDRA_CORE_VERSION_PATCH = "2";
+const std::string KLEPSYDRA_CORE_VERSION = "7.7.2";
